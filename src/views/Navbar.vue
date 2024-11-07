3ee48cf5344b367e2e77f01e9687a52eadf8a884
John <yangtianzhuo970425@gmail.com>
2024-11-08 12:39:48 +1300

    Implemented the test version of page navigation functionality

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -10,34 +10,34 @@
     <el-menu default-active="1" :class="collapse ? 'menu-bar-collapse-width' : 'menu-bar-width'" :collapse="collapse"
       :collapse-transition="false" :unique-opened="false" @open="handleOpen" @close="handleClose"
       @select="handleSelect">
-      <el-submenu index="1">
+      <el-submenu index="blog">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>Blogs</span>
         </template>
         <el-menu-item-group title="Category Management">
-          <el-menu-item index="1-1">List of Categories</el-menu-item>
+          <el-menu-item index="categories">List of Categories</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="Article Management">
-          <el-menu-item index="1-2">List of Articles</el-menu-item>
+          <el-menu-item index="posts">List of Articles</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <el-submenu index="2">
+      <el-submenu index="photography">
         <template slot="title">
           <i class="el-icon-location"></i>
           <span>Photography</span>
         </template>
         <el-menu-item-group title="Photo Management">
-          <el-menu-item index="2-1">List of Photos</el-menu-item>
+          <el-menu-item index="photos">List of Photos</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
       <el-menu-item index="3">
         <i class="el-icon-menu"></i>
         <span slot="title">Navigation 2</span>
       </el-menu-item>
-      <el-menu-item index="4">
+      <el-menu-item index="login">
         <i class="el-icon-setting"></i>
-        <span slot="title">Navigation 3</span>
+        <span slot="title">Login</span>
       </el-menu-item>
     </el-menu>
   </div>
@@ -61,10 +61,16 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
-    handleSelect(a, b) {
-      // console.log('handleSelect', a, b)
-      console.log('handleSelect', 'a', a)
-      console.log('handleSelect', 'b', b)
+    handleSelect(menuStr, menuArr) {
+      console.log('handleSelect', menuArr)
+      switch (menuArr[0]) {
+        case 'login':
+          this.$router.push('/login')
+          break
+        default:
+          console.log('default...')
+          break
+      }
     }
   }
 }
