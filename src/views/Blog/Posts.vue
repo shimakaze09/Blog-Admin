4bdf8fc3c0c8cc861675eaab2cfa39707ef68802
John <yangtianzhuo970425@gmail.com>
2024-11-08 14:58:59 +1300

    Article List Page Optimization

--- src/views/Blog/Posts.vue
+++ src/views/Blog/Posts.vue
@@ -1,42 +1,57 @@
 <template>
-  <div>
-    <el-row style="margin-bottom: 5px;">
-      <el-col :span="4">
-        <!-- Category Filter -->
-        <!-- Adding the filterable attribute to el-select enables search functionality. By default, Select will find options whose label contains the input value. -->
-        <el-select v-model="currentCategoryName" filterable placeholder="Please select"
-          v-on:change="handleCategoryChange">
-          <el-option v-for="item in categories" :key="item.id" :label="item.name" :value="item.id">
-          </el-option>
-        </el-select>
-      </el-col>
-    </el-row>
-    <!-- Setting height on the el-table element allows fixed table headers without additional code. -->
-    <el-table :data="posts" height="750" stripe style="width: 100%;margin-bottom: 10px;"
-      :default-sort="{ prop: 'lastUpdateTime', order: 'descending' }">
-      <el-table-column prop="id" label="ID" width="180">
-      </el-table-column>
-      <el-table-column prop="title" label="Title" sortable width="680">
-      </el-table-column>
-      <el-table-column prop="creationTime" label="Creation Time" sortable width="250">
-      </el-table-column>
-      <el-table-column prop="lastUpdateTime" label="Last Updated" sortable width="250">
-      </el-table-column>
-      <el-table-column prop="category.name" label="Category">
-      </el-table-column>
-      <el-table-column fixed="right" label="Operations" width="100">
-        <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">View</el-button>
-          <el-button type="text" size="small">Edit</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- Pagination -->
-    <el-pagination @size-change="handlePageSizeChange" @current-change="handleCurrentPageChange"
-      :current-page="currentPage" :page-sizes="[10, 20, 40, 60, 80, 100]" :page-size="pageSize" background
-      layout="total, sizes, prev, pager, next, jumper" :total="totalCount">
-    </el-pagination>
-  </div>
+  <el-container>
+    <el-header height="30px">
+      <el-row type="flex" justify="space-between">
+        <el-row gutter="10">
+          <el-col span="12">
+            <el-input placeholder="Enter keyword" prefix-icon="el-icon-search"></el-input>
+          </el-col>
+          <el-col span="12">
+            <!-- Category Filter -->
+            <!-- Adding the filterable attribute to el-select enables search functionality. By default, Select will find options whose label contains the input value. -->
+            <el-select v-model="currentCategoryName" filterable placeholder="Select category"
+              v-on:change="handleCategoryChange">
+              <el-option v-for="item in categories" :key="item.id" :label="item.name" :value="item.id">
+              </el-option>
+            </el-select>
+          </el-col>
+        </el-row>
+        <div>
+          <el-button>Add</el-button>
+          <el-button type="danger">Delete</el-button>
+        </div>
+      </el-row>
+    </el-header>
+    <el-main>
+      <!-- Setting height on the el-table element allows fixed table headers without additional code. -->
+      <el-table :data="posts" height="730" stripe style="width: 100%"
+        :default-sort="{ prop: 'lastUpdateTime', order: 'descending' }">
+        <el-table-column prop="id" label="ID" width="180">
+        </el-table-column>
+        <el-table-column prop="title" label="Title" sortable width="680">
+        </el-table-column>
+        <el-table-column prop="creationTime" label="Creation Time" sortable width="250">
+        </el-table-column>
+        <el-table-column prop="lastUpdateTime" label="Last Updated" sortable width="250">
+        </el-table-column>
+        <el-table-column prop="category.name" label="Category">
+        </el-table-column>
+        <el-table-column fixed="right" label="Operations" width="100">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">View</el-button>
+            <el-button type="text" size="small">Edit</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+    <el-footer height="30px">
+      <!-- Pagination -->
+      <el-pagination @size-change="handlePageSizeChange" @current-change="handleCurrentPageChange"
+        :current-page="currentPage" :page-sizes="[10, 20, 40, 60, 80, 100]" :page-size="pageSize" background
+        layout="total, sizes, prev, pager, next, jumper" :total="totalCount">
+      </el-pagination>
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
@@ -61,7 +76,7 @@ export default {
   },
   mounted() {
     // Load categories
-    this.$api.category.getList().then(res => {
+    this.$api.category.getAll().then(res => {
       let categories = [{ id: 0, name: 'All' }]
       categories = categories.concat(res)
       this.categories = categories
@@ -98,3 +113,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.el-header {
+  text-align: left;
+}
+</style>
