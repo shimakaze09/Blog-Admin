058f16aadfad3036fad5559dc99dfe51f5de7110
John <yangtianzhuo970425@gmail.com>
2024-11-08 13:39:30 +1300

    Optimize the display of the introduction page

--- src/views/Intro/Intro.vue
+++ src/views/Intro/Intro.vue
@@ -1,53 +1,38 @@
 <template>
   <div class="page-container" style="width:99%;margin-top:15px;">
-    <el-carousel :interval="3000" type="card" height="450px" class="carousel">
+    <el-carousel :interval="3000" type="card" height="550px" class="carousel">
       <el-carousel-item class="carousel-item-intro">
         <h2>Project Introduction</h2>
         <ul>
-          <li>A Java EE rapid development platform based on Spring Boot, Spring Cloud, Vue, and Element</li>
-          <li>Aims to provide a simple and easy-to-use solution to help users effectively reduce project development
-            difficulty and cost</li>
-          <li>The blog provides a series of tutorial articles on the project development process, guiding you through
-            how to develop similar systems</li>
+          <li>Based on Spring Boot, Spring Cloud, Vue, Element, Java EE rapid development platform</li>
+          <li>Aims to provide a concise and easy-to-use solution to help users effectively reduce project development difficulty and cost</li>
+          <li>The blog provides synchronous tutorial articles for the development process, teaching you how to develop similar systems step by step</li>
         </ul>
-        <div><img src="@/assets/logo.svg" style="width:120px;height:120px;padding-top:15px;" /></div>
+        <div><img src="@/assets/user.png" alt=""/></div>
       </el-carousel-item>
       <el-carousel-item class="carousel-item-func">
         <h2>Function Plan</h2>
         <ul>
-          <li>✔ System Login: User login system authentication (token method)</li>
-          <li>✔ User Management: Create, modify, delete, query users</li>
-          <li>✔ Organization Management: Create, modify, delete, query organizations</li>
-          <li>✔ Role Management: Create, modify, delete, query roles</li>
-          <li>✔ Menu Management: Create, modify, delete, query menus</li>
-          <li>✔ System Log: Record user operation logs, view system execution log records</li>
-          <li>✔ Data Monitoring: Customize Druid information, provide concise and effective SQL monitoring</li>
-          <li>✔ Aggregated Documentation: Customize online documentation, provide concise and beautiful API
-            documentation</li>
-          <li>✔ Backup Recovery: System backup recovery, one-click restore initial data</li>
-          <li>✔ Theme Switching: Support theme switching, customize color schemes, one-click skin change</li>
-          <li>✔ Service Governance: Integrate Spring Cloud, implement comprehensive service governance</li>
-          <li>✔ Service Monitoring: Integrate Spring Boot Admin, implement microservice monitoring</li>
+          <li>Core framework: Spring Boot 2.x. Service governance: Spring Cloud.</li>
+          <li>View framework: Spring MVC 5.x. Persistence layer framework: MyBatis 3.x.</li>
+          <li>Database connection pool: Druid 1.x. Security framework: Shiro 1.4.x.</li>
+          <li>Front-end framework: Vue.js 2.x. Page components: Element 2.x.</li>
+          <li>Status management: Vuex.js 2.x. Backend interaction: axios 0.18.x.</li>
           <li>...</li>
         </ul>
+        <div><img src="@/assets/codelab.png" alt=""></div>
       </el-carousel-item>
       <el-carousel-item class="carousel-item-env">
         <h2>Development Environment</h2>
         <ul>
-          <li>IDE : eclipse 4.6.x. JDK : JDK 1.8.x.</li>
-          <li>Maven : Maven 3.5.x. MySQL: MySQL 5.7.x.</li>
-          <li>IDE : VS Code 1.27. Webpack：webpack 3.2.x.</li>
-          <li>NODE: Node 8.9.x. NPM : NPM 6.4.x.</li>
-        </ul>
-        <h2>Technology Selection</h2>
-        <ul>
-          <li>Main Framework: Spring Boot 2.x. Service Governance: Spring Cloud.</li>
-          <li>View Framework: Spring MVC 5.x. Persistence Layer Framework: MyBatis 3.x.</li>
-          <li>Database Connection Pool: Druid 1.x. Security Framework: Shiro 1.4.x.</li>
-          <li>Front-end Framework: Vue.js 2.x. Page Components: Element 2.x.</li>
-          <li>State Management: Vuex.js 2.x. Backend Interaction: axios 0.18.x.</li>
+          <li>Core framework: Spring Boot 2.x. Service governance: Spring Cloud.</li>
+          <li>View framework: Spring MVC 5.x. Persistence layer framework: MyBatis 3.x.</li>
+          <li>Database connection pool: Druid 1.x. Security framework: Shiro 1.4.x.</li>
+          <li>Front-end framework: Vue.js 2.x. Page components: Element 2.x.</li>
+          <li>Status management: Vuex.js 2.x. Backend interaction: axios 0.18.x.</li>
           <li>...</li>
         </ul>
+        <div><img src="@/assets/star.png" alt=""></div>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -55,10 +40,8 @@
 
 <script>
 export default {
-  components: {
-  },
-  methods: {
-  }
+  components: {},
+  methods: {}
 }
 </script>
 
@@ -67,6 +50,12 @@ export default {
   padding-left: 20px;
   padding-right: 20px;
   margin-right: 20px;
+}
+
+.carousel img {
+  width: 200px;
+  height: 200px;
+  padding-top: 15px;
 }
 
 .carousel h2 {
