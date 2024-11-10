a3464fc0e44bb696a9a8fd51623880b33b2a5d7f
John <yangtianzhuo970425@gmail.com>
2024-11-10 17:00:34 +1300

    Enhance category interface

--- src/http/modules/category.js
+++ src/http/modules/category.js
@@ -1,5 +1,6 @@
 import axios from "../axios"
 
+// Get all categories
 export const getAll = () => {
   return axios({
     url: 'Category/All',
@@ -15,9 +16,33 @@ export const getList = (page = 1, pageSize = 10) => {
   })
 }
 
-export const get = categoryId => {
+// Get a specific category
+export const get = itemId => {
   return axios({
-    url: `Category/${categoryId}`,
+    url: `Category/${itemId}`,
     method: 'get'
+  })
+}
+
+// Generate category word cloud
+export const wordCloud = () => {
+  return axios({
+    url: 'Category/WordCloud'
+  })
+}
+
+// Set a category as featured
+export const setFeatured = itemId => {
+  return axios({
+    url: `Category/${itemId}/SetFeatured`,
+    method: 'post'
+  })
+}
+
+// Cancel a category's featured status
+export const cancelFeatured = itemId => {
+  return axios({
+    url: `Category/${itemId}/CancelFeatured`,
+    method: 'post'
   })
 }
