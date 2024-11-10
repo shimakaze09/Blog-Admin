fa51110b4c51f0e611149d4e9a0278308a55177d
John <yangtianzhuo970425@gmail.com>
2024-11-10 16:40:55 +1300

    Improve the Blog Post API

--- src/http/modules/blogPost.js
+++ src/http/modules/blogPost.js
@@ -10,41 +10,41 @@ export const getList = (categoryId = 0, page = 1, pageSize = 20) => {
 }
 
 // Get specific blog post
-export const get = postId => {
+export const get = itemId => {
   return axios({
-    url: `BlogPost/${postId}/`,
+    url: `BlogPost/${itemId}/`,
     method: 'get',
   })
 }
 
 // Delete a specific blog post
-export const deletePost = postId => {
+export const deleteItem = itemId => {
   return axios({
-    url: `BlogPost/${postId}/`,
+    url: `BlogPost/${itemId}/`,
     method: 'delete'
   })
 }
 
 // Set a blog post as featured
-export const setFeatured = postId => {
+export const setFeatured = itemId => {
   return axios({
-    url: `BlogPost/${postId}/SetFeatured`,
+    url: `BlogPost/${itemId}/SetFeatured`,
     method: 'post'
   })
 }
 
 // Cancel a blog post's featured status
-export const cancelFeatured = postId => {
+export const cancelFeatured = itemId => {
   return axios({
-    url: `BlogPost/${postId}/CancelFeatured`,
+    url: `BlogPost/${itemId}/CancelFeatured`,
     method: 'post'
   })
 }
 
 // Set a blog post as top
-export const setTop = postId => {
+export const setTop = itemId => {
   return axios({
-    url: `BlogPost/${postId}/SetTop`,
+    url: `BlogPost/${itemId}/SetTop`,
     method: 'post'
   })
 }
