2e91b480aa9a7f1619413cbd7b44c09235d73a70
John <yangtianzhuo970425@gmail.com>
2024-11-11 12:56:33 +1300

    Complete image API

--- src/http/modules/photo.js
+++ src/http/modules/photo.js
@@ -1,17 +1,19 @@
 import axios from '../axios'
 
+// Get List
 export const getList = (page = 1, pageSize = 10) => {
   return axios({
     url: 'Photo',
     method: 'get',
-    params: { page, pageSize }
+    params: {page, pageSize}
   })
 }
 
+// Get Specific Item
 export const get = photoId => {
   return axios({
     url: `Photo/${photoId}/`,
-    method: 'get',
+    method: 'get'
   })
 }
 
@@ -25,7 +27,31 @@ export const add = (title, location, file) => {
   return axios({
     url: 'Photo',
     method: 'post',
-    headers: { 'Content-Type': 'multipart/form-data;charset=UTF-8' },
+    headers: {'Content-Type': 'multipart/form-data;charset=UTF-8'},
     data: formData
+  })
+}
+
+// Delete Specific Item
+export const deleteItem = itemId => {
+  return axios({
+    url: `Photo/${itemId}/`,
+    method: 'delete'
+  })
+}
+
+// Set Featured
+export const setFeatured = itemId => {
+  return axios({
+    url: `Photo/${itemId}/SetFeatured`,
+    method: 'post'
+  })
+}
+
+// Cancel Featured
+export const cancelFeatured = itemId => {
+  return axios({
+    url: `Photo/${itemId}/CancelFeatured`,
+    method: 'post'
   })
 }
