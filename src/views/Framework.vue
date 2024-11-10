6df8a4f6e7d11fc4ebeffbce6b0120da13060dea
John <yangtianzhuo970425@gmail.com>
2024-11-10 15:22:45 +1300

    Architecture Reconfiguration

---
+++ src/views/Framework.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <!-- Navigation Bar -->
+    <navbar></navbar>
+    <!-- Header Area -->
+    <header-bar></header-bar>
+    <!-- Main Content Area -->
+    <main-content></main-content>
+  </div>
+</template>
+
+<script>
+import HeaderBar from "./HeaderBar"
+import Navbar from "./Navbar"
+import MainContent from "./MainContent"
+
+export default {
+  // name: 'Home',
+  components: {
+    HeaderBar,
+    Navbar,
+    MainContent
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
