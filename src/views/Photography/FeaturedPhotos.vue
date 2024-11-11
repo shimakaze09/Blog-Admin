88706df2b12effc699df7588e2f957150ef55405
John <yangtianzhuo970425@gmail.com>
2024-11-11 15:26:08 +1300

    Complete Featured Photo Management

--- src/views/Photography/FeaturedPhotos.vue
+++ src/views/Photography/FeaturedPhotos.vue
@@ -1,10 +1,85 @@
 <template>
-  <el-empty description="Recommended Photos"></el-empty>
+  <el-container>
+    <el-header height="30px">
+      <el-row type="flex" justify="start">
+        <div>
+          <el-button @click="">Add</el-button>
+        </div>
+      </el-row>
+    </el-header>
+    <el-main>
+      <el-row v-if="featuredPhotos.length > 0" :gutter="10">
+        <el-col :span="4" v-for="fp in featuredPhotos" :key="fp.id">
+          <el-card :body-style="{ padding: '0px' }">
+            <el-image :src="fp.photo.url" class="image" alt="" :preview-src-list="[fp.photo.url]"></el-image>
+            <div style="padding: 14px;">
+              <div>Image Title: {{ fp.photo.title }}</div>
+              <div>Location: {{ fp.photo.location }}</div>
+              <div style="margin-top: 3px;">
+                <time class="time">{{ fp.photo.dateTimeStr }}</time>
+              </div>
+              <div class="bottom clearfix" style="margin-top: 3px;">
+                <el-button-group>
+                  <el-button type="info" icon="el-icon-warning-outline" plain @click="notImpl"></el-button>
+                  <el-button type="primary" icon="el-icon-edit" plain @click="notImpl"></el-button>
+                  <el-button type="danger" icon="el-icon-delete" plain @click="deleteItem(fp)"></el-button>
+                </el-button-group>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-empty v-else description="Featured Photos"></el-empty>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+import { baseUrl } from "@/utils/global"
+
 export default {
-  name: "FeaturedPhotos"
+  name: "FeaturedPhotos",
+  components: {},
+  data() {
+    return {
+      featuredPhotos: [],
+    }
+  },
+  mounted() {
+    this.loadData()
+  },
+  methods: {
+    loadData() {
+      this.$api.featuredPhoto.getAll().then(res => {
+        console.log(res)
+        this.featuredPhotos = res.data
+        this.featuredPhotos.forEach(item => {
+          item.photo.url = `${baseUrl}/media/photography/${item.photo.id}.jpg`
+          let dt = new Date(item.photo.createTime)
+          item.photo.dateTimeStr = `${dt.toLocaleDateString()} ${dt.toLocaleTimeString()}`
+        })
+        console.log('featuredPhotos', this.featuredPhotos)
+      })
+    },
+    notImpl() {
+      this.$message('Not implemented yet!')
+    },
+    deleteItem(featuredPhoto) {
+      this.$confirm('This operation will delete this featured photo. Do you want to continue?', 'Warning', {
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        this.$api.featuredPhoto.deleteItem(featuredPhoto.id)
+          .then(res => {
+            this.$message.success(`Deleted successfully. ${res.message}`)
+            // Delete completed, reload data
+            this.loadData()
+          })
+          .catch(res => this.$message.error(`Operation failed. ${res.message}`))
+      }).catch(() => this.$message('Delete cancelled'))
+    }
+  }
 }
 </script>
 
