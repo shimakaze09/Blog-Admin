347efe24fb068df0608dac67fd736a8473ecc915
John <yangtianzhuo970425@gmail.com>
2024-11-11 18:53:46 +1300

    Modify the layout of the recommended photos page

--- src/views/Photography/FeaturedPhotos.vue
+++ src/views/Photography/FeaturedPhotos.vue
@@ -1,37 +1,26 @@
 <template>
-  <el-container>
-    <el-header height="30px">
-      <el-row type="flex" justify="start">
-        <div>
-          <el-button @click="">Add</el-button>
+  <el-row v-if="featuredPhotos.length > 0" :gutter="10">
+    <el-col :span="4" v-for="fp in featuredPhotos" :key="fp.id">
+      <el-card :body-style="{ padding: '0px' }">
+        <el-image :src="fp.photo.url" class="image" alt="" :preview-src-list="[fp.photo.url]"></el-image>
+        <div style="padding: 14px;">
+          <div>Photo Title: {{ fp.photo.title }}</div>
+          <div>Location: {{ fp.photo.location }}</div>
+          <div style="margin-top: 3px;">
+            <time class="time">{{ fp.photo.dateTimeStr }}</time>
+          </div>
+          <div class="bottom clearfix" style="margin-top: 3px;">
+            <el-button-group>
+              <el-button type="info" icon="el-icon-warning-outline" plain @click="notImpl"></el-button>
+              <el-button type="primary" icon="el-icon-edit" plain @click="notImpl"></el-button>
+              <el-button type="danger" icon="el-icon-delete" plain @click="deleteItem(fp)"></el-button>
+            </el-button-group>
+          </div>
         </div>
-      </el-row>
-    </el-header>
-    <el-main>
-      <el-row v-if="featuredPhotos.length > 0" :gutter="10">
-        <el-col :span="4" v-for="fp in featuredPhotos" :key="fp.id">
-          <el-card :body-style="{ padding: '0px' }">
-            <el-image :src="fp.photo.url" class="image" alt="" :preview-src-list="[fp.photo.url]"></el-image>
-            <div style="padding: 14px;">
-              <div>Image Title: {{ fp.photo.title }}</div>
-              <div>Location: {{ fp.photo.location }}</div>
-              <div style="margin-top: 3px;">
-                <time class="time">{{ fp.photo.dateTimeStr }}</time>
-              </div>
-              <div class="bottom clearfix" style="margin-top: 3px;">
-                <el-button-group>
-                  <el-button type="info" icon="el-icon-warning-outline" plain @click="notImpl"></el-button>
-                  <el-button type="primary" icon="el-icon-edit" plain @click="notImpl"></el-button>
-                  <el-button type="danger" icon="el-icon-delete" plain @click="deleteItem(fp)"></el-button>
-                </el-button-group>
-              </div>
-            </div>
-          </el-card>
-        </el-col>
-      </el-row>
-      <el-empty v-else description="Featured Photos"></el-empty>
-    </el-main>
-  </el-container>
+      </el-card>
+    </el-col>
+  </el-row>
+  <el-empty v-else description="Featured Photos"></el-empty>
 </template>
 
 <script>
@@ -65,8 +54,8 @@ export default {
       this.$message('Not implemented yet!')
     },
     deleteItem(featuredPhoto) {
-      this.$confirm('This operation will delete this featured photo. Do you want to continue?', 'Warning', {
-        confirmButtonText: 'Confirm',
+      this.$confirm('Are you sure you want to delete this featured photo?', 'Warning', {
+        confirmButtonText: 'OK',
         cancelButtonText: 'Cancel',
         type: 'warning'
       }).then(() => {
