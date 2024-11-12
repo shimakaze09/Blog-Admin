964270dd76cb44f6c33eef9f18b32f57f89addd3
John <yangtianzhuo970425@gmail.com>
2024-11-12 13:31:55 +1300

    Fix production environment address variable

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '',
-  EXCEPTION_LESS_API_KEY: '1L1zCqr7PW4JnKlpz7Lr4CgYIdLAf7CDn01TtxFM',
+  BASE_URL: '""',
+  EXCEPTION_LESS_API_KEY: '"1L1zCqr7PW4JnKlpz7Lr4CgYIdLAf7CDn01TtxFM"',
 }
