93cbdc250f10c5376c597172abc3b0a9e04870a0
John <yangtianzhuo970425@gmail.com>
2024-11-12 13:41:21 +1300

    Add Visit Statistics Interface

---
+++ src/http/modules/visitRecord.js
@@ -0,0 +1,37 @@
+import axios from '../axios'
+
+// Get list
+export const getList = (page = 1, pageSize = 10) => {
+  return axios({
+    url: 'VisitRecord',
+    method: 'get',
+    params: { page, pageSize }
+  })
+}
+
+// Get specific item
+export const get = photoId => {
+  return axios({
+    url: `VisitRecord/${photoId}/`,
+    method: 'get',
+  })
+}
+
+// Get all items
+export const getAll = () => {
+  return axios({ url: 'VisitRecord/All' })
+}
+
+// Get overview data
+export const getOverview = () => {
+  return axios({ url: 'VisitRecord/Overview' })
+}
+
+// Get statistics
+export const getStats = (date = null) => {
+  return axios({
+    url: 'VisitRecord/Stats',
+    method: 'get',
+    params: date === null ? null : { ...date }
+  })
+}
