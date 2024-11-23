490321f01d0692186fa544924449a8521e519c86
John <yangtianzhuo970425@gmail.com>
2024-11-23 17:19:29 +1300

    Optimize the article editing interface

--- src/views/Blog/EditPost.vue
+++ src/views/Blog/EditPost.vue
@@ -1,24 +1,53 @@
 <template>
   <div class="pb-5">
-    <el-input v-model="postTitle" placeholder="Post Title"></el-input>
+    <el-breadcrumb class="my-3" separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">Home</el-breadcrumb-item>
+      <el-breadcrumb-item>Blogs</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/post/list' }">Post Management</el-breadcrumb-item>
+      <el-breadcrumb-item v-if="mode==='edit'">Edit Post</el-breadcrumb-item>
+      <el-breadcrumb-item v-if="mode==='new'">New Post</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-divider></el-divider>
+
+    <div class="d-flex justify-content-between mb-1">
+      <div>Title</div>
+      <el-link target="_blank" type="primary">{{ `${baseUrl}/Blog/Post/${post.id}` }}</el-link>
+    </div>
+    <el-input v-model="postTitle" class="mb-3" placeholder="Article Title"></el-input>
+
+    <div class="mb-1">Content</div>
 
     <v-md-editor v-model="postContent" :codemirror-style-reset="true" :default-show-toc="true"
                  :disabled-menus="mode === 'edit' ? [] : ['image/upload-image']"
-                 class="mb-3" height="750px"
+                 class="mb-4" height="750px"
                  @save="onEditorSave" @fullscreen-change="fullscreenChange" @upload-image="handleUploadImage"/>
 
-    <el-form :model="form" class="text-start mt-5" label-position="top" label-width="80px">
-      <el-form-item label="Post Options" prop="isPublish">
+    <el-divider></el-divider>
+
+    <el-descriptions :column="2" border title="Article Information">
+      <el-descriptions-item label="Article ID">{{ post.id }}</el-descriptions-item>
+      <el-descriptions-item label="Status">
+        <el-tag size="small">{{ post.status }}</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="Creation Time">{{ post.creationTime }}</el-descriptions-item>
+      <el-descriptions-item label="Last Update Time">{{ post.lastUpdateTime }}</el-descriptions-item>
+    </el-descriptions>
+
+    <el-divider content-position="left">Article Options</el-divider>
+
+    <el-form :model="form" class="text-start" label-position="left" label-width="auto">
+      <el-form-item label="Mode" prop="isPublish">
         <el-radio v-model="form.isPublish" :label="true">Publish</el-radio>
         <el-radio v-model="form.isPublish" :label="false">Save as Draft</el-radio>
       </el-form-item>
 
       <el-row :gutter="20">
         <el-col :span="12">
-          <el-form-item
-            label="URL Slug (Friendly URL, only letters, numbers, hyphens, and underscores, up to 150 characters)"
-            prop="slug">
+          <el-form-item label="Slug" prop="slug">
             <el-input v-model="form.slug" maxlength="150" placeholder="Slug" show-word-limit></el-input>
+            <small>(Friendly URL name, can only use letters, numbers, hyphens (-), underscores (_), and must not exceed
+              150 characters)</small>
           </el-form-item>
         </el-col>
         <el-col :span="12">
@@ -38,13 +67,17 @@
       </el-form-item>
     </el-form>
 
-    <el-button plain type="primary" @click="onSaveClick">Save</el-button>
+    <div class="text-end">
+      <el-button :loading="saveButtonLoading" plain type="primary" @click="onSaveClick">Save</el-button>
+    </div>
 
   </div>
 </template>
 
 <script>
 // TODO: Add a page close warning or auto-save feature
+import {baseUrl} from "@/utils/global";
+import {dateTimeBeautify} from "@/utils/dateTime";
 
 import VMdEditor from '@kangc/v-md-editor';
 import enUS from '@kangc/v-md-editor/lib/lang/en-US';
@@ -55,6 +88,7 @@ export default {
   name: "EditPost",
   data() {
     return {
+      baseUrl: baseUrl,
       // Edit mode: new / edit
       mode: 'new',
       postTitle: '',
@@ -67,7 +101,8 @@ export default {
         slug: '',
         summary: '',
         category: 0,
-      }
+      },
+      saveButtonLoading: false,
     }
   },
   mounted() {
@@ -107,6 +142,8 @@ export default {
             this.post = res.data
             this.postTitle = this.post.title
             this.postContent = this.post.content
+            this.post.creationTime = dateTimeBeautify(this.post.creationTime)
+            this.post.lastUpdateTime = dateTimeBeautify(this.post.lastUpdateTime)
             this.form.slug = this.post.slug
             this.form.isPublish = this.post.isPublish
             this.form.summary = this.post.summary
@@ -155,10 +192,12 @@ export default {
       post.summary = this.form.summary
       post.categoryId = this.form.category
 
+      this.saveButtonLoading = true
+
       if (this.mode === 'new') {
         this.$api.blogPost.add(post)
           .then(res => {
-            this.$message.success(`Save successful. ${res.message}`)
+            this.$message.success(`Save successful.`)
             // Redirect to edit page after saving
             this.$router.push(`/post/edit/${res.data.id}`)
           })
@@ -166,10 +205,12 @@ export default {
             console.log(res)
             this.$message.error(`Operation failed. ${res.message}`)
           })
+          .finally(() => this.saveButtonLoading = false)
       } else {
         this.$api.blogPost.update(post)
-          .then(res => this.$message.success(`Saved successfully. ${res.message}`))
+          .then(res => this.$message.success(`Saved successfully.`))
           .catch(res => this.$message.error(`Operation failed. ${res.message}`))
+          .finally(() => this.saveButtonLoading = false)
       }
     }
   }
